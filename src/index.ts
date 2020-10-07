069b64475c3b8ba57935c119f51f4b28ac6c3195
ankurk91 <pro.ankurk1@gmail.com>
2020-10-07 16:14:19 +0530

    add vue types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import WebStorage from './webStorage';
 import StorageWithEvents from './storageWithEvents';
 import {arrayify} from './util';
 import {driverType} from "./interfaces";
+import {Vue, VueConstructor} from 'vue/types/vue'
+import {PluginFunction, PluginObject} from 'vue'
 
 const registerInstance = (Vue: any, driver: driverType, prefix: string) => {
   let instance = new StorageWithEvents(prefix, driver);
@@ -11,7 +13,12 @@ const registerInstance = (Vue: any, driver: driverType, prefix: string) => {
   Vue.prototype[apiName] = instance;
 };
 
-const Plugin = (Vue: any, options: any = {}) => {
+export interface PluginOptions {
+  prefix?: string
+  drivers?: driverType[]
+}
+
+const Plugin = (Vue: any, options: PluginOptions = {}) => {
 
   let safeOptions = Object.assign({}, {
     prefix: 'app_',
@@ -22,6 +29,22 @@ const Plugin = (Vue: any, options: any = {}) => {
     registerInstance(Vue, driver, safeOptions.prefix);
   });
 };
+
+export interface VueWebStoragePlugin extends PluginObject<PluginOptions> {
+  install: PluginFunction<PluginOptions>
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    readonly $sessionStorage: StorageWithEvents
+    readonly $localStorage: StorageWithEvents
+  }
+
+  interface VueConstructor {
+    readonly $sessionStorage: StorageWithEvents
+    readonly $localStorage: StorageWithEvents
+  }
+}
 
 export default Plugin;
 export {WebStorage, StorageWithEvents, Plugin}
